efa76344e27180b140eb2c467f464363d6de1410
iMatt <115056575+themattery@users.noreply.github.com>
2024-07-31 20:15:53 -0300

    find kth node from end

---
+++ Find Kth Node From End/main.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+  
+  
+        
+def find_kth_from_end(linkedList, k):
+    fast = slow = linkedList.head
+    for _ in range(k):
+        if fast is None:
+            return None
+        fast = fast.next
+    while fast:
+        slow = slow.next
+        fast = fast.next
+    return slow
