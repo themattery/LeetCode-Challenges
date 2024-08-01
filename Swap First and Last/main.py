f8130e86ac75e20de9d0f7fc7d2f14a1b4e0e573
iMatt <115056575+themattery@users.noreply.github.com>
2024-08-01 08:05:14 -0300

    swap first and last

---
+++ Swap First and Last/main.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+        
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def swap_first_last(self):
+        if self.length == 0 or self.length == 1:
+            return None
+        head = self.head.value
+        tail = self.tail.value
+        self.head.value = tail
+        self.tail.value = head
+        return True
+        
