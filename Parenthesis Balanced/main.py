166a07d0ae84ec466d163537633b5ab90fa02749
Matheus Barbosa <115056575+themattery@users.noreply.github.com>
2024-10-06 19:57:49 -0300

    parenthesis balanced using stack

---
+++ Parenthesis Balanced/main.py
@@ -0,0 +1,40 @@
+class Stack:
+    def __init__(self):
+        self.stack_list = []
+
+    def print_stack(self):
+        for i in range(len(self.stack_list)-1, -1, -1):
+            print(self.stack_list[i])
+
+    def is_empty(self):
+        return len(self.stack_list) == 0
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list[-1]
+
+    def size(self):
+        return len(self.stack_list)
+
+    def push(self, value):
+        self.stack_list.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list.pop()
+
+
+
+def is_balanced_parentheses(string):
+    my_stack = Stack()
+    for char in string:
+        if char == "(":
+            my_stack.push(char)
+        elif char == ")":
+            if my_stack.is_empty() or my_stack.pop() != "(":
+                return False
+    return my_stack.is_empty()
